76b97d83bcce2d058c91467e277512e32d7a9028
Ian McDaniel <ianmcdainel@gmail.com>
2015-12-04 14:54:29 -0800

    add default blueprint

--- blueprints/ember-tml/index.js
+++ blueprints/ember-tml/index.js
@@ -2,7 +2,6 @@ module.exports = {
   description: '',
   normalizeEntityName: function() {},
   afterInstall: function() {
-    return this.addBowerPackageToProject('https://github.com/translationexchange/tml-js-browser.git','develop');
-    //return this.addBowerPackageToProject('tml-js'); 
+    return this.addBowerPackageToProject('tml-js');
   }
 };
