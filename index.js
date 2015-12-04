383795b99f89d816e8037df699ad7abf820a369d
Ian McDaniel <ianmcdainel@gmail.com>
2015-12-04 13:12:02 -0800

    use bower for tml-js

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-tml'
+  name: 'ember-tml',
+
+  included: function(app) {
+    this.app.import(app.bowerDirectory + '/tml-js/dist/tml.min.js');
+  }
 };
