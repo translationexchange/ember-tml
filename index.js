dd75680fafced2d6144be9197d332e432e87f420
Ian McDaniel <ianmcdainel@gmail.com>
2016-10-27 14:40:33 -0700

    added super to include block

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
   name: 'ember-tml',
 
   included: function(app) {
-    this.app.import(app.bowerDirectory + '/tml-js/dist/tml.min.js');
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/tml-js/dist/tml.min.js');
   }
 };
